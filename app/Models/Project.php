1e215474e3656c832305019d7054622c2ce1db86
Diego Palacio <doyola@netgate.com>
2023-09-29 10:54:43 -0500

    Added Project and Task model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Project extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+        /**
+        * The attributes that are mass assignable.
+        *
+        * @var array<int, string>
+        */
+
+        protected $fillable = [
+            'title',
+            'description',
+            'start_date',
+            'end_date',
+            'task_id'
+        ];
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Task extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'status'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
 }
